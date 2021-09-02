2d9dbfcd85b8ad782ac441b65499f9e245f65d47
Darrick Smith <smithwebdd@gmail.com>
2021-09-01 21:17:16 -0400

    applied authenticated_root designation to auth user

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   authenticated :user do
-    root 'pages#my_todo_items'
+    root 'pages#my_todo_items', as: :authenticated_root
   end
   root 'pages#home'
 end
