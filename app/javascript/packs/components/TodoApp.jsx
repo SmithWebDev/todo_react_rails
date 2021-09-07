6b578086fbb3b6a4a2f4315349667e6924712acd
Darrick Smith <smithwebdd@gmail.com>
2021-09-07 00:27:21 -0400

    added componentDidMount and getTodoItems function to the todoapp.jsx file

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -1,13 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import axios from 'axios'
+
 class TodoApp extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       todoItems: []
-    }
+    };
+    this.getTodoItems = this.getTodoItems.bind(this);
   }
+
+  componentDidMount(){
+    this.getTodoItems();
+  }
+
+  getTodoItems() {
+    axios
+      .get("/api/v1/todo_items")
+      .then(response => {
+        const TodoItems = response.data;
+        this.setState({ TodoItems });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
   render() {
     return  <p>TodoApp</p>
   }
