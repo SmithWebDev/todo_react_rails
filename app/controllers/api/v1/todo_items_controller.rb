0224fde0933883445089b9e2db4b5845608460a6
Darrick Smith <smithwebdd@gmail.com>
2021-09-06 20:20:56 -0400

    created private method for handing unauthorized and authorized access to todo_items, updated index action to use current_user helper, added before_action to log in user

--- app/controllers/api/v1/todo_items_controller.rb
+++ app/controllers/api/v1/todo_items_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::TodoItemsController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_todo_item, only: %i[show edit update destroy]
   def index
-    @todo_items = TodoItem.all
+    @todo_items = current_user.todo_items.all
   end
 
   def show; end
@@ -16,5 +17,17 @@ class Api::V1::TodoItemsController < ApplicationController
 
   def set_todo_item
     @todo_item = TodoItem.find(params[:id])
+  end
+
+  def authorized?
+    @todo_item.user == current_user
+  end
+
+  def handle_unauthorized
+    unless authorized?
+      respond_to do |format|
+        format.json { render :unauthorized, status: 401 }
+      end
+    end
   end
 end
