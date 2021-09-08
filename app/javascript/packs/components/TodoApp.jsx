7f3b2cc8a6f1c2e3c9248d811e64ba6540d840d2
Darrick Smith <smithwebdd@gmail.com>
2021-09-08 02:21:15 -0400

    created TodoForm to receive new todo items, added form to TodoApp

--- app/javascript/packs/components/TodoApp.jsx
+++ app/javascript/packs/components/TodoApp.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import axios from 'axios'
 import TodoItems from './TodoItems'
 import TodoItem from './TodoItem'
+import TodoForm from './TodoForm'
 
 class TodoApp extends React.Component {
   constructor(props) {
@@ -12,6 +13,7 @@ class TodoApp extends React.Component {
       todoItems: []
     };
     this.getTodoItems = this.getTodoItems.bind(this);
+    this.createTodoItem = this.createTodoItem.bind(this);
   }
 
   componentDidMount() {
@@ -30,13 +32,20 @@ class TodoApp extends React.Component {
       });
   }
 
+  createTodoItem(todoItem) {
+    const todoItems = [todoItem, ...this.state.todoItems];
+    this.setState({todoItems});
+  }
   render() {
     return (
-      <TodoItems>
-        {this.state.todoItems.map(todoItem => (
-          <TodoItem key={todoItem.id} todoItem={todoItem}/>
-        ))}
-      </TodoItems>
+      <>
+        <TodoForm createTodoItem={this.createTodoItem}/>
+        <TodoItems>
+          {this.state.todoItems.map(todoItem => (
+            <TodoItem key={todoItem.id} todoItem={todoItem}/>
+          ))}
+        </TodoItems>
+      </>
     );
   }
 }
