6068ad610117ef24865bb7f271c8d731bbbada80
Darrick Smith <smithwebdd@gmail.com>
2021-09-08 22:05:53 -0400

    added handleDestroy method to TodoItem component

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import axios from 'axios'
+import setAxiosHeaders from './AxiosHeaders'
 
 class TodoItem extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       complete: this.props.todoItem.complete,
+    };
+    this.handleDestroy = this.handleDestroy.bind(this)
+    this.path = `/api/v1/todo_items/${this.props.todoItem.id}`;
+  }
+
+  handleDestroy() {
+    setAxiosHeaders()
+    const confirmaion = confirm('Are you sure?')
+    if(confirmaion) {
+      axios
+        .delete(this.path)
+        .then(response => {
+          this.props.getTodoItems()
+        })
+        .catch(error => {
+          console.log(error)
+        })
     }
   }
+
   render() {
     const { todoItem } = this.props
+
     return (
       <tr className={`${this.state.complete ? 'table-light' : ''}`}>
         <td>
@@ -60,7 +81,12 @@ class TodoItem extends React.Component {
               Complete?
             </label>
           </div>
-          <button className="btn btn-outline-danger">Delete</button>
+          <button 
+            onClick={this.handleDestroy}
+            className="btn btn-outline-danger"
+          >
+            Delete
+          </button>
         </td>
       </tr>
     )
@@ -71,4 +97,5 @@ export default TodoItem
 
 TodoItem.propTypes = {
   todoItem: PropTypes.object.isRequired,
+  getTodoItems: PropTypes.func.isRequired
 }
